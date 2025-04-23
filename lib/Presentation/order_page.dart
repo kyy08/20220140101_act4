a268e2e5826d31723c1ff6db11559e01b39c50e4
kyy08 <kyyelzz1@gmail.com>
2025-04-23 21:15:42 +0700

    membuat fungsi untuk menghitung totalharga

--- lib/Presentation/order_page.dart
+++ lib/Presentation/order_page.dart
@@ -14,4 +14,13 @@ class _OrderPageState extends State<OrderPage> {
   final TextEditingController jumlahMakananController = TextEditingController();
   final TextEditingController jumlahMinumanController = TextEditingController();
 
-  
+  int totalHarga = 0;
+
+  void calculateTotalPrice() {
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
