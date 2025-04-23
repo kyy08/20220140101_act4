c2bf3df1d785e089d999394ce55230bb404aba82
kyy08 <kyyelzz1@gmail.com>
2025-04-23 21:18:55 +0700

    Membuat form order dengan validasi dan navigasi ke halaman detail

--- lib/Presentation/order_page.dart
+++ lib/Presentation/order_page.dart
@@ -24,3 +24,80 @@ class _OrderPageState extends State<OrderPage> {
       totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
     });
   }
+
+  @override
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(title: Text('Order Page')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          spacing: 16,
+          children: [
+            TextFormField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: 'Food Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your food order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: minumanController,
+              decoration: const InputDecoration(labelText: 'Drink Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your drink order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: jumlahMakananController,
+              decoration: const InputDecoration(labelText: 'Food QTY Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your qty of food order';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: jumlahMinumanController,
+              decoration: const InputDecoration(labelText: 'Drink QTY Order'),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your qty of drink order';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  calculateTotalPrice();
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailOrderPage(
+                        jumlahMakanan: jumlahMakananController.text,
+                        jumlahMinuman: jumlahMinumanController.text,
+                        makanan: makananController.text,
+                        minuman: minumanController.text,
+                        totalHarga: totalHarga,
+                      ),
+                    ),
+                  );
+                }
+              },
+              child: Text('Order Now'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
