941d4eda3a44dddc9e1ea6a8733253390a899e26
kyy08 <kyyelzz1@gmail.com>
2025-04-23 21:03:04 +0700

    membuat Homepage menggunakan statefulwidget

---
+++ lib/Presentation/home_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
